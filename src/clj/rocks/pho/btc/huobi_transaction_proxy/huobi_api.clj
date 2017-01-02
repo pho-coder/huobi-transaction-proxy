927ee91f6ec77d54ae899f1953006790dc324d41
Phoenix Zhang <pho.coderzhang@gmail.com>
2017-01-02 13:42:53 +0800

    fix

--- src/clj/rocks/pho/btc/huobi_transaction_proxy/huobi_api.clj
+++ src/clj/rocks/pho/btc/huobi_transaction_proxy/huobi_api.clj
@@ -14,13 +14,12 @@
                       "&method=get_account_info&secret_key="
                       secret_key)
         sign (digest/md5 sign-str)]
-    (json/read-str (:body (http-client/post "https://api.huobi.com/apiv3"
-                                            {:headers {"Content-Type" "application/x-www-form-urlencoded"}
-                                             :form-params {:method "get_account_info"
-                                                           :access_key access_key
-                                                           :created unix-time
-                                                           :sign sign}}))
-                   :key-fn keyword)))
+    (:body (http-client/post "https://api.huobi.com/apiv3"
+                             {:headers {"Content-Type" "application/x-www-form-urlencoded"}
+                              :form-params {:method "get_account_info"
+                                            :access_key access_key
+                                            :created unix-time
+                                            :sign sign}}))))
 
 (defn buy
   "buy by amount & price"
